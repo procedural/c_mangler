29e03fd4c175aff812b4f323376152e81968aa36
Constantine Tarasenkov <constantine@protonmail.com>
2018-04-02 11:37:16 +0300

    Search for *.c and *.h files in subfolders

--- c_mangler.sh
+++ c_mangler.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd "$(dirname -- "$(readlink -fn -- "${0}")")"
-
 echo "#pragma once" > /tmp/c_mangler.h
 for name in `nm --no-sort --defined-only --extern-only "${1}" | cut -d ' ' -f3`; do
   echo "#define $name _`openssl rand -hex 32`" >> /tmp/c_mangler.h
 done
-
-for file in *.c *.h; do
+IFS=$'\n'; set -f
+for file in `find . -name "*.c" -or -name "*.h"`; do
   if [ "`head -n 1 "${file}"`" != '#include "/tmp/c_mangler.h"' ]; then
     echo '#include "/tmp/c_mangler.h"' > /tmp/c_mangler_temp_file
     cat "${file}" >> /tmp/c_mangler_temp_file
     mv /tmp/c_mangler_temp_file "${file}"
   fi
 done
+unset IFS; set +f
